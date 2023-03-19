416243aaad64b2b71167612efa5c633d45d1f5c0
moses odhiambo <modhiambo056@gmail.com>
2023-03-19 16:12:03 +0300

    first mpesa app

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
+const port = process.env.PORT || 5000
+const mpesaRoutes = require("./routes/mpesa")
+dotenv.config()
+ 
+mongoose.connect(process.env.MONG0_URL)
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Application is running on port ${port} and DB is connected`)
+        })
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+app.use(express.json())
+app.use("/api", mpesaRoutes);
+
+
+
+
+
